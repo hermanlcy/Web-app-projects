cf860aa789731cb7728ca6f584f2d1757e8e284c
hermanlcy <61773482+hermanlcy@users.noreply.github.com>
2022-05-04 14:53:24 +0930

    Create app.js

---
+++ Wiki-API/app.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-lai:Test123@cluster0.smx2v.mongodb.net/wikiDB");
+
+const articlesSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articlesSchema);
+
+app.route("/articles")
+.get(function(req, res) {
+  Article.find({}, function(err, foundArticles) {
+    if(!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+.post(function(req, res) {
+  const newArticle = new Article ({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err) {
+    if(!err) {
+      res.send("Successfully added a new article!");
+    } else {
+      res.send(err);
+    }
+  });
+})
+.delete(function(req, res) {
+  Article.deleteMany({}, function(err) {
+    if(!err) {
+      res.send("Successfully deleted all article!");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// Requests Targetting a specific article
+
+app.route("/articles/:articleTitle")
+.get(function(req, res) {
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle) {
+    if(foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching with this title was found.");
+    }
+  });
+})
+.put(function(req, res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    function(err) {
+      if(!err) {
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+.patch(function(req, res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err) {
+      if(!err) {
+        res.send("Successfully updated article.")
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+.delete(function(req, res) {
+  Article.deleteOne({title: req.params.articleTitle}, function(err) {
+    if(!err) {
+      res.send("Successfully deleted an article!");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+})
